c86e17d7168167a7aa01136c5c846e71c142f5f1
Sam Sheffres <ssheffres@gmail.com>
2014-09-21 12:42:50 +0700

    Use kMeans clustering and add job title example data and results

--- clustering.py
+++ clustering.py
@@ -7,6 +7,8 @@ from nltk.cluster import KMeansClusterer, GAAClusterer, euclidean_distance
 import nltk.corpus
 from nltk import decorators
 import nltk.stem
+
+import codecs
  
 stemmer_func = nltk.stem.snowball.EnglishStemmer().stem
 stopwords = set(nltk.corpus.stopwords.words('english'))
@@ -38,14 +40,14 @@ if __name__ == '__main__':
     with open(filename) as title_file:
  
         print "Reading Files"
-        job_titles = [line.strip() for line in title_file.readlines()]
+        job_titles = [unicode(line.strip(), "utf-8") for line in title_file.readlines()]
  
         print "Parsing Words"
         words = get_words(job_titles)
  
-        #cluster = KMeansClusterer(17000, 5)
         print "Creating Cluster Instance"
-        cluster = GAAClusterer(20)
+        cluster = KMeansClusterer(10, euclidean_distance, 5)
+        #cluster = GAAClusterer(20)
         
         print "Clustering"
         cluster.cluster([vectorspaced(title) for title in job_titles if title])
@@ -59,5 +61,5 @@ if __name__ == '__main__':
         print "Saving results"
         for cluster_id, title in sorted(zip(classified_examples, job_titles)):
             filename = "results/"+ str(cluster_id) + ".txt"
-            list = open(filename, "a")
+            list = codecs.open(filename, "a", "utf-8")
             list.write(title + "\n")
